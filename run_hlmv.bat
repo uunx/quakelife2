95e6179a5d1565135c653bf41a289e02980d968b
unknown <cory@cory-PC.(none)>
2012-06-12 11:59:49 -0400

    first commit

---
+++ run_hlmv.bat
@@ -0,0 +1,2 @@
+@"c:\program files (x86)\steam\steamapps\cvc@hvc.rr.com\sourcesdk\bin\source2007\bin\hlmv.exe" -game "c:\program files (x86)\steam\steamapps\SourceMods\quake-life2" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
