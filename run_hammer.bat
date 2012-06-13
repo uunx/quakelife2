fb93b9cfbf9145e9ac8067d5d598bc3d652198b0
Zeuez <dat_email@email.com>
2012-06-13 15:03:44 -0400

    .gitignore is now working

--- run_hammer.bat
+++
@@ -1,2 +0,0 @@
-@"c:\program files (x86)\steam\steamapps\cvc@hvc.rr.com\sourcesdk\bin\source2007\bin\hammer.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
-
--- run_mod.bat
+++
@@ -1,1 +0,0 @@
-@"c:\program files (x86)\steam\steamapps\cvc@hvc.rr.com\source sdk base 2007\hl2.exe" -dev -game "c:\program files (x86)\steam\steamapps\SourceMods\quake-life2" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
